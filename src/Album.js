78da85671c33a3bcade76ddc503d54ffba82f4ba
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-12-02 19:34:34 +0530

    first commit

---
+++ src/Album.js
@@ -0,0 +1,257 @@
+import React, {useEffect, useState, useCallback, Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import * as firebase from "firebase";
+import firebaseConfig from './firebase.config';
+import { useHistory } from 'react-router';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Person from '@material-ui/icons/Person';
+import Avatar from '@material-ui/core/Avatar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+   
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.main,
+    padding: theme.spacing(6),
+  },
+  root: {
+    flexGrow: 1,
+     backgroundColor: '#121212',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const cards = [1, 2, 3];
+
+
+export default function Album() {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  useEffect(() => {
+    const user = firebase.auth();
+    firebase.auth().onAuthStateChanged(user => {
+      history.push(user ? '/dashboard' : '/');
+    })
+  }, []);
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },  
+      }),
+    [prefersDarkMode],
+  );
+
+
+  function onClick() {
+    setTimeout(
+        function() {
+            try {
+                firebase.auth().signOut();
+                history.push('/');
+            } 
+            catch (e) {
+                console.log(e);
+            }
+        }, 2000);
+  }
+  return (
+
+    <ThemeProvider theme={theme}>
+        <React.Fragment >
+      <CssBaseline />
+      <AppBar position="relative">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Microblog
+                </Typography>
+                <IconButton edge="start" className={classes.menuButtonProfile} color="inherit" aria-label="menu">
+                    <Person />
+                </IconButton>&nbsp;&nbsp;
+                <Button onClick={onClick} variant="contained" color="secondary">
+                    Logout
+                </Button>
+            </Toolbar>
+     </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={handleClickOpen}>
+                    Book Tickets
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Write post</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                What's on your mind? Write a blog on something that excites you.
+            </DialogContentText>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="title"
+                label="Title"
+                type="text"
+                fullWidth
+            />
+            <TextField
+                autoFocus
+                margin="dense"
+                id="content"
+                label="Content"
+                type="paragraph"
+                fullWidth
+                multiline
+                rows={4}
+            />
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+                Post
+            </Button>
+            </DialogActions>
+        </Dialog>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+    </ThemeProvider>
+    
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,207 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState, useCallback, Component} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Person from '@material-ui/icons/Person';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import * as firebase from "firebase";
+import firebaseConfig from './firebase.config';
+import { BrowserRouter, Route,Switch} from 'react-router-dom';
+import { useHistory } from 'react-router';
+import SignupScreen from './SignupScreen';
+import Album from './Album';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const SignIn = (props) => {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = firebase.auth();
+    firebase.auth().onAuthStateChanged(user => {
+      history.push(user ? '/dashboard' : '/');
+    })
+  }, []);
+
+  function onClick() {
+    console.log(email, password);
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(res => {
+      if (res.user) {
+        //Auth.setLoggedIn(true);
+        console.log(res);
+        history.push('/dashboard');
+      }
+    })
+    .catch(e => {
+      console.log("error message", e.message);
+    });
+  }
+
+
+  function onClickGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Person />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onClick}
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onClickGoogle}
+            color="secondary"
+          >
+            Google
+          </Button><br /><br />
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link onClick={() => history.push('/signup') } variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+class Stack extends Component{
+  render () {
+    return (
+      <BrowserRouter>
+          <Switch>
+            <Route exact={true} path="/" component={SignIn} />
+            <Route exact={true} path="/dashboard" component={Album} />
+            <Route exact={true} path="/signup" component={SignupScreen} />
+          </Switch>
+     </BrowserRouter>
+    );
+  }
+}
+
+export default Stack;
